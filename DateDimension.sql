e7889baf9a5c841f1e3d106ea1913c3cf5078dfd
Radhika Takwale <rtakw072@uottawa.ca>
2022-03-19 18:33:12 -0400

    Update DateDimension.sql
    Updated constraints and columns of Date dimension

--- DateDimension.sql
+++ DateDimension.sql
@@ -9,16 +9,16 @@ CREATE TABLE public.date
     day_num integer,
     week_num integer,
     month_num integer,
-    year integer,
     season text COLLATE pg_catalog."default",
-    quarter integer,
     day_of_week text COLLATE pg_catalog."default",
+    year_num integer,
+    quarter integer,
     CONSTRAINT date_pkey PRIMARY KEY (date_key),
-    CONSTRAINT day_num CHECK (day_num >= 1 AND day_num <= 366),
     CONSTRAINT month_num CHECK (month_num >= 1 AND month_num <= 12),
+    CONSTRAINT day_num CHECK (day_num >= 1 AND day_num <= 366),
+    CONSTRAINT year_num CHECK (year_num >= 2005 AND year_num <= 2020),
     CONSTRAINT quarter CHECK (quarter >= 1 AND quarter <= 4),
-    CONSTRAINT week_num CHECK (week_num >= 1 AND week_num <= 52),
-    CONSTRAINT year CHECK (year >= 2005 AND year <= 2020)
+    CONSTRAINT week_num CHECK (week_num >= 1 AND week_num <= 53)
 )
 WITH (
     OIDS = FALSE
@@ -27,9 +27,3 @@ TABLESPACE pg_default;
 
 ALTER TABLE public.date
     OWNER to rtakw072;
-
-GRANT ALL ON TABLE public.date TO cvenc066;
-
-GRANT ALL ON TABLE public.date TO eassa062;
-
-GRANT ALL ON TABLE public.date TO rtakw072;
